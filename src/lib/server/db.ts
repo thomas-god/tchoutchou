18016ef7eb422ddcb847d520c95d8224ae9b4d22
thomas-god <thomas.godlewski@icloud.com>
2026-01-29 10:34:41 +0100

    feat: add basic persistence layer with sqlite

---
+++ src/lib/server/db.ts
@@ -0,0 +1,19 @@
+import { DatabaseSync } from 'node:sqlite';
+import { getEnv } from '$lib/env';
+
+let _db: DatabaseSync | null = null;
+
+export const getDb = () => {
+	if (!_db) {
+		const path = getEnv('VITE_DATABASE_PATH');
+		_db = new DatabaseSync(path);
+
+		_db.exec(`
+      PRAGMA journal_mode = WAL;
+    `);
+	}
+
+	return _db;
+};
+
+export const initDB = (db: DatabaseSync) => {};
