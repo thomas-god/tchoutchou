56c3cf6f45a6032fa2d601e549f7153d3a17d669
thomas-god <thomas.godlewski@icloud.com>
2026-03-11 18:02:22 +0100

    chore(client): remove station merging page

--- client/src/routes/stations/+page.ts
+++
@@ -1,7 +0,0 @@
-import { fetchMergeCandidates } from '$lib/remote/mergeCandidate.remote';
-
-export const prerender = false;
-
-export const load = () => {
-	return { groups: fetchMergeCandidates({ maxDistanceKm: 1.0 }) };
-};
