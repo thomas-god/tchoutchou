42b3c7b963910bc9165616c0c9c3e234877e6256
thomas-god <thomas.godlewski@icloud.com>
2026-03-14 21:11:27 +0100

    feat(client): remove navbar

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<nav class="flex gap-4 border-b border-base-300 bg-base-100 px-6 py-3">
-	<a href="/" class="font-semibold hover:text-primary">Home</a>
-</nav>
 {@render children()}
